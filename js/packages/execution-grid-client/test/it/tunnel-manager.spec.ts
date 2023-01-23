6496812831122481a0f00f0e18e95bb392feb71b
kyrylo <kyrylo.onufriiev@applitools.com>
2023-01-23 17:57:59 +0200

    [eg-client] add tunnel pool test

--- js/packages/execution-grid-client/test/it/tunnel-manager.spec.ts
+++ js/packages/execution-grid-client/test/it/tunnel-manager.spec.ts
@@ -113,4 +113,34 @@ describe('tunnel-manager', () => {
       (err: Error) => err.message.includes('TUNNEL_NOT_FOUND'),
     )
   })
+
+  it('acquires tunnels', async () => {
+    const manager = await makeTunnelManager({settings: {serverUrl: 'http://eg-tunnel'}, logger: makeLogger()})
+
+    nock('http://eg-tunnel').persist().post('/tunnels').reply(201, '"tunnel-id"')
+
+    const [{tunnelId}] = await manager.acquire({eyesServerUrl: 'http://eyes-server', apiKey: 'api-key'})
+
+    assert.strictEqual(tunnelId, 'tunnel-id')
+  })
+
+  it('acquires tunnels that were previously created', async () => {
+    const manager = await makeTunnelManager({settings: {serverUrl: 'http://eg-tunnel'}, logger: makeLogger()})
+
+    let tunnelIndex = 0
+    nock('http://eg-tunnel')
+      .persist()
+      .post('/tunnels')
+      .reply(() => [201, `"tunnel-id-${tunnelIndex++}"`])
+    nock('http://eg-tunnel')
+      .persist()
+      .delete(/\/tunnels\/tunnel-id-\d+/)
+      .reply(200)
+
+    const tunnels = await manager.acquire({eyesServerUrl: 'http://eyes-server', apiKey: 'api-key'})
+    await manager.release(tunnels)
+    const reusedTunnels = await manager.acquire({eyesServerUrl: 'http://eyes-server', apiKey: 'api-key'})
+
+    assert.deepStrictEqual(reusedTunnels, tunnels)
+  })
 })
