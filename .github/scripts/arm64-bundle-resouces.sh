19e349fa10368245d0228449d7630041c0d86525
kyrylo <kyrylo.onufriiev@applitools.com>
2023-02-03 17:05:34 +0200

    yarn fix lint and build commands

--- .github/scripts/arm64-bundle-resouces.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -
-set -o nounset
-find ../eyes-universal-arm64 -type f -not -name '*.zip' -not -name '*.tar.gz' -exec bash -c 'cp ${0} ./bin/$(basename ${0})' {} \;
-rm -rf ./bin/*.tar.gz ./bin/*.zip
-zip -j ./bin/eyes-universal.zip $(find ./bin -type f -not -name '*.zip' -not -name '*.tar.gz' | xargs)
-tar -czf ./bin/eyes-universal.tar.gz $(find ./bin -type f -not -name '*.zip' -not -name '*.tar.gz' | xargs)
