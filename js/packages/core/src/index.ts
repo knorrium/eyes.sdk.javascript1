ef7668feb2a48802c53aab5716c4e12822a55feb
kyrylo <kyrylo.onufriiev@applitools.com>
2023-02-16 15:43:46 +0200

    [core] export spec type

--- js/packages/core/src/index.ts
+++ js/packages/core/src/index.ts
@@ -3,7 +3,7 @@ export * from './core'
 export * from './universal/core-server'
 export * from './universal/core-server-process'
 
-export {type Selector} from '@applitools/driver'
+export {type SpecType, type SpecDriver, type Selector} from '@applitools/driver'
 export * from '@applitools/driver/dist/debug'
 
 export * as formatters from './utils/format-results'
