f2690f72e9e7d660c0683423e72f04fad97768cd
kyrylo <kyrylo.onufriiev@applitools.com>
2023-02-22 17:54:34 +0200

    [core] add test reporting

---
+++ js/packages/core/.mocharc.cvg.js
@@ -0,0 +1,14 @@
+const {mochaGrep} = require('@applitools/test-utils')
+
+module.exports = {
+  spec: ['./test/**/*.spec.ts'],
+  exclude: ['./test/bin/**'],
+  parallel: true,
+  jobs: 15,
+  timeout: 0,
+  exit: true,
+  require: ['ts-node/register'],
+  reporter: 'mocha-multi',
+  reporterOptions: ['spec=-,xunit=coverage-test-report.xml'],
+  grep: mochaGrep(),
+}
