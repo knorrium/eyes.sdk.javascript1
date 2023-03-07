17d89df3cfaa2683788078bb23fa617b84131321
kyrylo <kyrylo.onufriiev@applitools.com>
2023-03-07 18:07:54 +0200

    [api] add Resize value to StitchMode enum

--- js/packages/eyes-api/src/enums/StitchMode.ts
+++ js/packages/eyes-api/src/enums/StitchMode.ts
@@ -1,6 +1,9 @@
 export enum StitchModeEnum {
   SCROLL = 'Scroll',
+  Scroll = 'Scroll',
   CSS = 'CSS',
+  RESIZE = 'Resize',
+  Resize = 'Resize',
 }
 
 export type StitchMode = `${StitchModeEnum}`
