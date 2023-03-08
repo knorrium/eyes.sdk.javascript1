54f26dae2a329389e1ba6799ae87eb93d501e47d
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2023-03-07 17:43:14 -0800

    [py] run playwright functional tests on CI (#1322)
    * Rename eyes_selenium_unit_tests target to unit_tests as it runs unit tests in all modules
    * Simplify playwright functional tests
    * Add eyes_playwright_functional_tests make target
    * Run playwright functional tests during other functional tests execution

--- python/tests/functional/eyes_playwright/test_eyes_playwright.py
+++ python/tests/functional/eyes_playwright/test_eyes_playwright.py
@@ -1,57 +1,43 @@
+import pytest
 from playwright.sync_api import sync_playwright
 
-from applitools.selenium import Eyes
-from applitools.selenium.runner import PlaywrightRunner
+from applitools.playwright import Eyes
 
 
-def test_playwright_eyes_check_non_fully():
+@pytest.fixture
+def page():
     with sync_playwright() as p:
         browser = p.chromium.launch()
-        page = browser.new_page()
-        page.goto("http://whatsmyuseragent.org/")
-        runner = PlaywrightRunner()
-        eyes = Eyes(runner)
-        eyes.configure.set_viewport_size({"width": 800, "height": 600})
-        eyes.open(page, "Playwright", "Eyes check non-fully")
-        eyes.check_window(fully=False)
-        eyes.close()
+        yield browser.new_page()
 
 
-def test_playwright_eyes_check_fully():
-    with sync_playwright() as p:
-        browser = p.chromium.launch()
-        page = browser.new_page()
-        page.goto("http://whatsmyuseragent.org/")
-        runner = PlaywrightRunner()
-        eyes = Eyes(runner)
-        eyes.configure.set_viewport_size({"width": 800, "height": 600})
-        eyes.open(page, "Playwright", "Eyes check fully")
-        eyes.check_window()
-        eyes.close()
+@pytest.fixture
+def eyes(request, page):
+    eyes = Eyes()
+    eyes.configure.set_viewport_size({"width": 800, "height": 600})
+    eyes.configure.set_app_name("eyes-playwright functional tests")
+    eyes.configure.set_test_name(request.node.name)
+    eyes.open(page)
+    yield eyes
+    eyes.close()
 
 
-def test_playwright_eyes_check_element_selector():
-    with sync_playwright() as p:
-        browser = p.chromium.launch()
-        page = browser.new_page()
-        page.goto("http://whatsmyuseragent.org/")
-        runner = PlaywrightRunner()
-        eyes = Eyes(runner)
-        eyes.configure.set_viewport_size({"width": 800, "height": 600})
-        eyes.open(page, "Playwright", "Eyes check element selector")
-        eyes.check_element(".user-agent")
-        eyes.close()
+def test_playwright_eyes_check_fully(eyes, page):
+    page.goto("https://demo.applitools.com")
+    eyes.check_window()
 
 
-def test_playwright_eyes_check_element_locator():
-    with sync_playwright() as p:
-        browser = p.chromium.launch()
-        page = browser.new_page()
-        page.goto("http://whatsmyuseragent.org/")
-        element = page.locator(".ip-address")
-        runner = PlaywrightRunner()
-        eyes = Eyes(runner)
-        eyes.configure.set_viewport_size({"width": 800, "height": 600})
-        eyes.open(page, "Playwright", "Eyes check element locator")
-        eyes.check_element(element)
-        eyes.close()
+def test_playwright_eyes_check_non_fully(eyes, page):
+    page.goto("https://demo.applitools.com")
+    eyes.check_window(fully=False)
+
+
+def test_playwright_eyes_check_element_selector(eyes, page):
+    page.goto("https://demo.applitools.com")
+    eyes.check_element("form")
+
+
+def test_playwright_eyes_check_element_locator(eyes, page):
+    page.goto("https://demo.applitools.com")
+    element = page.locator("form")
+    eyes.check_element(element)
