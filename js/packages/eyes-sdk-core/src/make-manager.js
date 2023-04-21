e7f76671b194f973f8e45c527952d53a0b5b91e8
Ran Itzhaki <ran.itzhaki@applitools.com>
2023-04-21 15:33:53 +0300

    fix: Refactor `makeManager` function to accept settings object (#1460)
    * fix: Refactor `makeManager` function to accept settings object
    
    This commit refactors the `makeManager` function in `make-manager.js` to accept a `settings` object instead of separate `concurrency` and `legacyConcurrency ` parameters. The function now merges the `settings` object with optional properties `concurrency` and `legacyConcurrency` using the spread operator.
    
    * PR Comments

--- js/packages/eyes-sdk-core/src/make-manager.js
+++ js/packages/eyes-sdk-core/src/make-manager.js
@@ -3,7 +3,8 @@ const makeCloseManager = require('./close-manager')
 
 function makeMakeManager({core}) {
   return async function makeManager({type, concurrency, legacyConcurrency} = {}) {
-    const manager = await core.makeManager({type: type === 'vg' ? 'ufg' : type, concurrency, legacyConcurrency})
+    const settings = {concurrency, legacyConcurrency}
+    const manager = await core.makeManager({type: type === 'vg' ? 'ufg' : type, settings})
 
     return {
       openEyes: makeOpenEyes({core, manager}),
