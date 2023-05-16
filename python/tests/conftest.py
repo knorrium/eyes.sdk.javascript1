bc30648ed0e2339d712d34aa656e146efe04f79c
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2023-05-15 21:53:33 -0700

    [py] report generated tests to separate packages (#1534)

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -5,9 +5,10 @@ import re
 import uuid
 from contextlib import closing
 from gzip import GzipFile
-from os import environ, path, remove
+from os import environ, getcwd, path, remove
 from subprocess import check_call, check_output
 from sys import platform
+from typing import Optional, Tuple
 from xml.etree import ElementTree
 
 import pytest
@@ -23,22 +24,39 @@ environ["APPLITOOLS_DONT_CLOSE_BATCHES"] = "true"
 
 @pytest.hookimpl(trylast=True)
 def pytest_sessionfinish(session, exitstatus):
+    try:
+        from selenium import __version__ as selenium_version
+    except ImportError:
+        selenium_version = "4"
     if environ.get("BONGO_REPORT_TESTS", "false").lower() not in ("false", "0"):
         releasing_package = environ.get("RELEASING_PACKAGE")
         npx = "npx.cmd" if platform == "win32" else "npx"
-        if releasing_package == "eyes_robotframework":
-            reported_name = "robotframework"
-        else:  # unless releasing robotframework package, report all tests as python sdk
-            reported_name = "python"
         git_sha = check_output(["git", "log", "-n1", "--format=%H"]).decode().strip()
-        cmd = [npx, "bongo", "report", "--name", reported_name, "--reportId", git_sha]
-        if path.exists("coverage-tests-metadata.json"):
+        is_generic = path.exists("coverage-tests-metadata.json")
+        suite_dir = path.basename(getcwd())
+        selenium_version = int(selenium_version.split(".", 1)[0])
+        reported_name, group = sdk_name_group(
+            is_generic, selenium_version, suite_dir, releasing_package
+        )
+        cmd = [
+            npx,
+            "bongo",
+            "report",
+            "--verbose",
+            "--reportId",
+            git_sha,
+            "--name",
+            reported_name,
+        ]
+        if group:
+            cmd += ["--group", group]
+        if is_generic:
             remove_test_name_prefix("coverage-test-report.xml")
             cmd += ["--metaDir", "."]
         if not releasing_package:
             cmd.append("--sandbox")
         flush = {} if PY2 else {"flush": True}
-        print("\n\nReporting tests for package:", reported_name, **flush)
+        print("\n\nReporting tests:", " ".join(cmd), **flush)
         check_call(cmd)
     remove("coverage-test-report.xml")
 
@@ -94,3 +112,77 @@ def remove_test_name_prefix(xml_report_path):
         cleaned_name = re.sub(r"^(test_)?(.+?)(@sauce_(mac_)?vm_\d+)?$", r"\2", name)
         testcase.set("name", cleaned_name)
     return xml.write(xml_report_path, encoding="unicode", xml_declaration=True)
+
+
+def sdk_name_group(is_generic, selenium_version, suite_dir, releasing_package):
+    # type: (bool, int, str, Optional[str]) -> Tuple[str, Optional[str]]
+    group_name = "images" if is_generic and suite_dir == "images" else None
+    if releasing_package == "eyes_robotframework":
+        default_sdk_name = "robotframework"
+    else:
+        default_sdk_name = "python"
+
+    if is_generic:
+        sdk_name_map = {
+            "appium": {3: "python_selenium_3", 4: default_sdk_name},
+            "selenium": {3: "python_selenium_3", 4: default_sdk_name},
+            "playwright": {4: "python_playwright"},
+            "images": {3: default_sdk_name, 4: default_sdk_name},
+        }
+        sdk_name = sdk_name_map[suite_dir][selenium_version]
+        return sdk_name, group_name
+    else:
+        return default_sdk_name, group_name
+
+
+def test_sdk_name_group():
+    # releaseing eyes_selenium
+    assert sdk_name_group(True, 4, "images", "eyes_selenium") == ("python", "images")
+    assert sdk_name_group(True, 3, "appium", "eyes_selenium") == (
+        "python_selenium_3",
+        None,
+    )
+    assert sdk_name_group(True, 4, "appium", "eyes_selenium") == ("python", None)
+    assert sdk_name_group(True, 3, "selenium", "eyes_selenium") == (
+        "python_selenium_3",
+        None,
+    )
+    assert sdk_name_group(True, 4, "selenium", "eyes_selenium") == ("python", None)
+    assert sdk_name_group(True, 4, "playwright", "eyes_selenium") == (
+        "python_playwright",
+        None,
+    )
+
+    # releasing eyes_robotframework
+    assert sdk_name_group(True, 4, "images", "eyes_robotframework") == (
+        "robotframework",
+        "images",
+    )
+    assert sdk_name_group(True, 3, "appium", "eyes_robotframework") == (
+        "python_selenium_3",
+        None,
+    )
+    assert sdk_name_group(True, 4, "appium", "eyes_robotframework") == (
+        "robotframework",
+        None,
+    )
+    assert sdk_name_group(True, 3, "selenium", "eyes_robotframework") == (
+        "python_selenium_3",
+        None,
+    )
+    assert sdk_name_group(True, 4, "selenium", "eyes_robotframework") == (
+        "robotframework",
+        None,
+    )
+    assert sdk_name_group(True, 4, "playwright", "eyes_robotframework") == (
+        "python_playwright",
+        None,
+    )
+
+    # non-generic tests always reported to python/robotframework
+    assert sdk_name_group(False, 4, "any", None) == ("python", None)
+    assert sdk_name_group(False, 4, "any", "eyes_selenium") == ("python", None)
+    assert sdk_name_group(False, 4, "any", "eyes_robotframework") == (
+        "robotframework",
+        None,
+    )
