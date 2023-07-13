5609a36c93622c9b8eeb4b4ab25f95907df8baa4
kyrylo <kyrylo.onufriiev@applitools.com>
2023-07-13 18:22:37 +0300

    fix: prevent tunnel binaries from overriding itself

--- js/packages/tunnel-client/src/prepare-tunnel-environment.ts
+++ js/packages/tunnel-client/src/prepare-tunnel-environment.ts
@@ -3,16 +3,20 @@ import {type Logger} from '@applitools/logger'
 //@ts-ignore
 import {prepareEnvironment} from '@applitools/execution-grid-tunnel'
 
-export function prepareTunnelEnvironment({
+export async function prepareTunnelEnvironment({
   settings,
   logger,
 }: {
   settings?: PrepareTunnelEnvironmentSettings
   logger?: Logger
 }) {
-  return prepareEnvironment({
-    egTunnelManagerUrl: settings?.tunnelServerUrl,
-    customTestCacheDirectoryPath: settings?.cacheDir,
-    logger,
-  })
+  try {
+    await prepareEnvironment({
+      egTunnelManagerUrl: settings?.tunnelServerUrl,
+      customTestCacheDirectoryPath: settings?.cacheDir,
+      logger,
+    })
+  } catch (error: any) {
+    if (error.message !== 'PREPARE_ENV_ALREADY_RUNNING') throw error
+  }
 }
