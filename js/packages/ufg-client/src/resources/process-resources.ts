dca9eadd2bab39d1fc20b99d997879075691f0ee
kyrylo <kyrylo.onufriiev@applitools.com>
2023-07-13 15:42:39 +0300

    fix: fixed an issue when an inability to freeze a gif image caused sdk crush

--- js/packages/ufg-client/src/resources/process-resources.ts
+++ js/packages/ufg-client/src/resources/process-resources.ts
@@ -87,8 +87,12 @@ export function makeProcessResources({
   }): Promise<KnownResource> {
     if (utils.types.has(resource, 'value')) {
       if (/image\/gif/.test(resource.contentType)) {
-        logger.log('Freezing gif image resource')
-        resource = makeResource({...resource, value: await freezeGif(resource.value)})
+        try {
+          logger.log(`Freezing gif image resource with id ${resource.id}`)
+          resource = makeResource({...resource, value: await freezeGif(resource.value)})
+        } catch (error) {
+          logger.warn(`Failed to freeze gif image resource with id ${resource.id} due to an error`, error)
+        }
       }
     }
     return persistResource({resource, logger})
