096b7110ca4ad89659f3d90605be6d7d9b7b881b
IdosApplitools <ido.elmaleh@applitools.com>
2023-07-18 13:23:56 +0300

    chore(java): fix circular dependencies

--- java/eyes.universal.core/eyes-universal-core/build.sh
+++ java/eyes.universal.core/eyes-universal-core/build.sh
@@ -20,21 +20,21 @@ done
 # build current module
 mvn clean install -P "$1" -DskipTests
 
-POST_BUILDS=(
-  "../eyes-universal-core-binaries"
-  "../eyes-universal-core-alpine"
-  "../eyes-universal-core-arm"
-  "../eyes-universal-core-linux"
-  "../eyes-universal-core-mac"
-  "../eyes-universal-core-win"
-)
-
-# build dependant modules
-for build_path in "${POST_BUILDS[@]}"; do
-  echo "in $(pwd)"
-  echo "Executing build in: $build_path"
-  cd "$build_path" || exit
-  chmod +x "build.sh"
-  ./build.sh "$1"
-  cd - || exit
-done
+#POST_BUILDS=(
+#  "../eyes-universal-core-binaries"
+#  "../eyes-universal-core-alpine"
+#  "../eyes-universal-core-arm"
+#  "../eyes-universal-core-linux"
+#  "../eyes-universal-core-mac"
+#  "../eyes-universal-core-win"
+#)
+#
+## build dependant modules
+#for build_path in "${POST_BUILDS[@]}"; do
+#  echo "in $(pwd)"
+#  echo "Executing build in: $build_path"
+#  cd "$build_path" || exit
+#  chmod +x "build.sh"
+#  ./build.sh "$1"
+#  cd - || exit
+#done
--- java/eyes.universal.core/eyes-universal-core/runTests.sh
+++ java/eyes.universal.core/eyes-universal-core/runTests.sh
@@ -5,5 +5,10 @@ set -e;
 
 export APPLITOOLS_LOG_DIR="./reports/logs/";
 
-# Run module tests
-#mvn test -e -X;
+echo "Checking grep pattern: $MAVEN_GREP"
+if [ -n "$MAVEN_GREP" ]; then
+  mvn test -Dtest="$MAVEN_GREP" -e -X
+else
+  # Run the default suite file
+  mvn test -e -X
+fi
