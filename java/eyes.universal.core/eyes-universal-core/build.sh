1d733a58dd17376ee14ae13769afd57c8615411c
IdosApplitools <ido.elmaleh@applitools.com>
2023-07-19 10:20:25 +0300

    chore(java): manually build binaries cores

--- java/eyes.universal.core/eyes-universal-core/build.sh
+++ java/eyes.universal.core/eyes-universal-core/build.sh
@@ -2,9 +2,17 @@
 
 # module: eyes-universal-core
 # dependencies: [eyes-common-java5]
+# profiles: [eyes-universal-core-binaries, eyes-universal-core-alpine, eyes-universal-core-linux
+#            eyes-universal-core-arm, eyes-universal-core-mac, eyes-universal-core-win]
 
 PRE_BUILDS=(
   "../../eyes.common.java"
+  "../eyes-universal-core-binaries"
+  "../eyes-universal-core-alpine"
+  "../eyes-universal-core-arm"
+  "../eyes-universal-core-linux"
+  "../eyes-universal-core-mac"
+  "../eyes-universal-core-win"
 )
 
 # build dependant modules
@@ -19,22 +27,3 @@ done
 
 # build current module
 mvn clean install -P "$1" -DskipTests
-
-#POST_BUILDS=(
-#  "../eyes-universal-core-binaries"
-#  "../eyes-universal-core-alpine"
-#  "../eyes-universal-core-arm"
-#  "../eyes-universal-core-linux"
-#  "../eyes-universal-core-mac"
-#  "../eyes-universal-core-win"
-#)
-#
-## build dependant modules
-#for build_path in "${POST_BUILDS[@]}"; do
-#  echo "in $(pwd)"
-#  echo "Executing build in: $build_path"
-#  cd "$build_path" || exit
-#  chmod +x "build.sh"
-#  ./build.sh "$1"
-#  cd - || exit
-#done
