8c36fc3932591edd365ce04328c00aaed1fc1846
IdosApplitools <ido.elmaleh@applitools.com>
2023-07-20 10:40:00 +0300

    chore(java): improve gh download and explicitly build modules for alpine test

--- java/eyes.universal.core/eyes-universal-core-alpine/runTests.sh
+++ java/eyes.universal.core/eyes-universal-core-alpine/runTests.sh
@@ -5,9 +5,22 @@ set -e;
 
 export APPLITOOLS_LOG_DIR="./reports/logs/";
 
-# build eyes-universal-core before test
-chmod +x ../eyes-universal-core/build.sh
-../eyes-universal-core/build.sh;
+# build eyes-common-java and core-alpine before the test so it exist in maven local repo
+BUILDS=(
+  "../../eyes.common.java"
+)
+
+# build dependant modules
+for build_path in "${BUILDS[@]}"; do
+  echo "in $(pwd)"
+  echo "Executing build in: $build_path"
+  cd "$build_path" || exit
+  chmod +x "build.sh"
+  ./build.sh
+  cd - || exit
+done
+# build current module
+mvn clean install -DskipTests
 
 echo "Checking grep pattern: $MAVEN_GREP"
 # Run the default suite file
--- java/eyes.universal.core/eyes-universal-core-arm/runTests.sh
+++ java/eyes.universal.core/eyes-universal-core-arm/runTests.sh
@@ -8,11 +8,13 @@ export APPLITOOLS_LOG_DIR="./reports/logs/";
 
 # install github cli (gh)
 sudo apt update
-sudo apt install gh
-
-# build eyes-universal-core before test
-chmod +x ../eyes-universal-core/build.sh
-../eyes-universal-core/build.sh;
+sudo apt install wget -y
+# Download the gh binary for arm64 (for buildjet-2vcpu-ubuntu-2204-arm)
+wget https://github.com/cli/cli/releases/download/v2.20.2/gh_2.20.2_linux_arm64.tar.gz
+tar -xvf gh_2.20.2_linux_arm64.tar.gz
+sudo mv gh_2.20.2_linux_arm64/bin/gh /usr/bin/
+# Verify the installation
+gh --version
 
 echo "Checking grep pattern: $MAVEN_GREP"
 # Run the default suite file
