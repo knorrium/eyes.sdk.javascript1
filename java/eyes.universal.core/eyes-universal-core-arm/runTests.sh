4cac77dfd3dd2d89e5ae40f3e9c2b38ad727334c
IdosApplitools <ido.elmaleh@applitools.com>
2023-07-20 10:43:17 +0300

    chore(java): explicitly build modules in arm test to reduce build time

--- java/eyes.universal.core/eyes-universal-core-arm/runTests.sh
+++ java/eyes.universal.core/eyes-universal-core-arm/runTests.sh
@@ -16,6 +16,23 @@ sudo mv gh_2.20.2_linux_arm64/bin/gh /usr/bin/
 # Verify the installation
 gh --version
 
+# build eyes-common-java and core-arm before the test so it exist in maven local repo
+BUILDS=(
+  "../../eyes.common.java"
+)
+
+# build dependant modules
+for build_path in "${BUILDS[@]}"; do
+  echo "in $(pwd)"
+  echo "Executing build in: $build_path"
+  cd "$build_path" || exit
+  chmod +x "build.sh"
+  ./build.sh
+  cd - || exit
+done
+# build current module
+mvn clean install -DskipTests
+
 echo "Checking grep pattern: $MAVEN_GREP"
 # Run the default suite file
 if [ -n "$MAVEN_GREP" ]; then
