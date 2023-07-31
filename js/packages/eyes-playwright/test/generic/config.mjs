768da9bf229707e9b8dac43e9c5c5e40af064df1
kyrylo <kyrylo.onufriiev@applitools.com>
2023-07-31 16:30:19 +0300

    chore: update generic configs

--- js/packages/eyes-playwright/test/generic/config.mjs
+++ js/packages/eyes-playwright/test/generic/config.mjs
@@ -2,7 +2,10 @@ export const config = {
   extends: '../../../../test/generic/config.mjs',
   suite: test => {
     return (
-      (!test.env || ['chrome', 'chromium', 'firefox', 'webkit', 'safari'].includes(test.env.browser)) &&
+      (!test.env ||
+        (!test.env.device &&
+          !test.env.emulation &&
+          ['chrome', 'chromium', 'firefox', 'webkit', 'safari'].includes(test.env.browser))) &&
       !test.features?.includes('webdriver')
     )
   },
--- js/packages/eyes-puppeteer/test/generic/config.mjs
+++ js/packages/eyes-puppeteer/test/generic/config.mjs
@@ -1,6 +1,9 @@
 export const config = {
   extends: '../../../../test/generic/config.mjs',
   suite: test => {
-    return (!test.env || ['chrome', 'chromium'].includes(test.env.browser)) && !test.features.includes('webdriver')
+    return (
+      (!test.env || (!test.env.device && !test.env.emulation && ['chrome', 'chromium'].includes(test.env.browser))) &&
+      !test.features?.includes('webdriver')
+    )
   },
 }
